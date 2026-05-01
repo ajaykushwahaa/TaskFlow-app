02447052ce209a8c6faf3a72fb76fc4cc853c4a5
Ajay Kushwaha <binendrarajmaurya111@gmail.com>
2026-05-01 13:11:20 +0530

    update API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'taskflow-app-production-bb1b.up.railway.app',
   headers: { 'Content-Type': 'application/json' }
 });
 
